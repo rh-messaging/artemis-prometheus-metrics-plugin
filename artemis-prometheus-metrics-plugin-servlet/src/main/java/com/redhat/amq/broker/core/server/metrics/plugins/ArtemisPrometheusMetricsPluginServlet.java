ae1a36428e1d0cc23bb8e2c64a251b1e718fbd36
Andy Taylor <andy.tayls67@gmail.com>
2024-02-02 14:27:02 +0000

    upgraded to 3.0.0 and Jakarta

--- artemis-prometheus-metrics-plugin-servlet/src/main/java/com/redhat/amq/broker/core/server/metrics/plugins/ArtemisPrometheusMetricsPluginServlet.java
+++ artemis-prometheus-metrics-plugin-servlet/src/main/java/com/redhat/amq/broker/core/server/metrics/plugins/ArtemisPrometheusMetricsPluginServlet.java
@@ -17,10 +17,6 @@
 
 package com.redhat.amq.broker.core.server.metrics.plugins;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Set;
@@ -29,6 +25,10 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class ArtemisPrometheusMetricsPluginServlet extends HttpServlet {
 
